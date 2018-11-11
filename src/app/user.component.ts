23a9a26d1a9557342885d016d2cc9d285eaa92cf
Dimitris <kastrind@gmail.com>
2018-11-12 00:50:58 +0200

    Emitting and listening to custom events.

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector : 'app-user',
     template : `
-    <input type="text" [(ngModel)]="name">
+    <input type="text" (input)="onUserInput($event)" [value]="name">
+    <!--<input type="text" [(ngModel)]="name">-->
     <p>Hello {{name}}!</p>
     <p>I am the user component!</p>`
 })
 
 export class UserComponent {
     @Input() name;
+    @Output() nameChanged = new EventEmitter<string>();
 
     onUserInput(event) {
-      this.name = event.target.value;
+      // this.name = event.target.value;
+      this.nameChanged.emit(event.target.value);
     }
 }
