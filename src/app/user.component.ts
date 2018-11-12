1d1d6341f1d204ccccd6e186a1abeea0f33a3ab9
Dimitris <kastrind@gmail.com>
2018-11-13 01:37:49 +0200

    Component generation with CLI command ng c g component-name

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -6,7 +6,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <input type="text" (input)="onUserInput($event)" [value]="name">
     <!--<input type="text" [(ngModel)]="name">-->
     <p>Hello {{name}}!</p>
-    <p>I am the user component!</p>`
+    <p>I am the user component!</p>
+    <app-user-detail></app-user-detail>
+    `
 })
 
 export class UserComponent {
