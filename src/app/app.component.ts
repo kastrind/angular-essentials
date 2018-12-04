795e538a774e8eee4fbaa04aa67f1506a60adf10
Dimitrios Kastrinakis <kastrind@gmail.com>
2018-12-05 01:00:30 +0200

    Added ngFor structural directive usage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,17 @@ import { random } from 'lodash';
 export class AppComponent {
   title = 'angular-essentials';
   rootName = 'Bilbo';
+  names = ['Aragorn', 'Boromir', 'Legolas'];
   number = 0;
 
   onNameChanged(newName) {
     this.rootName = newName;
+    //example of a function imported from a js framework
     this.number = random(1, 10);
+  }
+
+  onNameAdded(newName) {
+    this.names.push(newName);
   }
 
 }
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -4,6 +4,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     selector : 'app-user',
     template : `
     <input type="text" (input)="onUserInput($event)" [value]="name">
+    <button class="btn" [ngClass]="{'btn-primary': name !== '', 'btn-default': name === ''}" (click)="onUserAdded($event)">Add</button>
+    <!--<input type="text" (input)="onUserInput($event)" [value]="name">-->
     <!--<input type="text" [(ngModel)]="name">-->
     <p>Hello {{name}}!</p>
     <p>I am the user component!</p>
@@ -14,9 +16,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class UserComponent {
     @Input() name;
     @Output() nameChanged = new EventEmitter<string>();
+    @Output() nameAdded = new EventEmitter<string>();
 
     onUserInput(event) {
       // this.name = event.target.value;
       this.nameChanged.emit(event.target.value);
+    }
+
+    onUserAdded() {
+      this.nameAdded.emit(this.name);
     }
 }
